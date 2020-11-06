cb12379f355ba5a546ddca6b6042defa968223c0
Sudheendra K <69519385+sudheendrak2@users.noreply.github.com>
2020-11-06 16:52:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+const number = document.querySelector('#txt1');
+const txt2 = document.querySelector('#txt2');
+const clear = document.querySelector('#btn1');
+const submit = document.querySelector('#btn2');
